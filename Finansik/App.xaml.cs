b75f3c0c1b9401a4698dbae250679d49fcb75899
User <User@WINDELL-JVD6IED>
2024-02-22 10:04:54 +0100

    Dodaj pliki projektów.

---
+++ Finansik/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Finansik
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
