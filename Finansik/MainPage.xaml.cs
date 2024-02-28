fd423b2066a17eee720c055408ac7aa461997dc6
HEAVY <rvrctvyb@gmail.com>
2024-02-28 14:35:59 +0100

    yooo

--- Finansik/MainPage.xaml.cs
+++ Finansik/MainPage.xaml.cs
@@ -1,6 +1,6 @@
 ﻿namespace Finansik
 {
-    string abc = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+    //string abc = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
     public partial class MainPage : ContentPage
     {
         string adnjdbibbc = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
