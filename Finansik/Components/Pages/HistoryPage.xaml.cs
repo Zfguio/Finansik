f3510b5cacd9720dca4898623fc5fe484248f692
Michał Kraus <rvrctvyb@gmial.com>
2024-03-23 13:09:23 +0100

    working history

--- Finansik/Components/Pages/HistoryPage.xaml.cs
+++ Finansik/Components/Pages/HistoryPage.xaml.cs
@@ -1,9 +1,44 @@
+using Finansik.Classes;
+using Finansik.Service;
+using Finansik.Service.DTO;
+using System.Collections.ObjectModel;
+using Finansik.Service.DTO;
 namespace Finansik.Components.Pages;
-
+ 
 public partial class HistoryPage : ContentPage
 {
-	public HistoryPage()
+    public class Fruit
+    {
+        public string FruitName { get; set; }
+    }
+    List<Finanse> finanses = new List<Finanse>();
+    ObservableCollection<Finanse> fruits = new ObservableCollection<Finanse>();
+    public ObservableCollection<Finanse> Fruits { get { return fruits; } }
+
+    public HistoryPage()
 	{
 		InitializeComponent();
-	}
+        loadTable();
+    }
+
+    void getValues() 
+    {
+        foreach (Finanse f in finanses)
+        {
+            fruits.Add(AddFinanse(f.Title, f.Price));
+        }
+        FruitListView.ItemsSource = fruits;
+    }
+    async void  loadTable() 
+    {
+        finanses = await TransatcionService.GetListAsync();
+        getValues();
+    }
+    Finanse AddFinanse(string title, float price) 
+    {
+        Finanse yep = new Finanse();
+        yep.Title=title;
+        yep.Price=price;
+        return yep;
+    }
 }
--- Finansik/Service/DTO/Finanse.cs
+++ Finansik/Service/DTO/Finanse.cs
@@ -1,7 +1,7 @@
 ﻿using SQLite;
 namespace Finansik.Service.DTO
 {
-    class Finanse
+    public class Finanse
     {
         [PrimaryKey, AutoIncrement]
         public int Id { get; set; } 
