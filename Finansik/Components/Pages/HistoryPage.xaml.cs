7b1b8659efa80679c56b1a252ab5789da8134a88
Michał Kraus <rvrctvyb@gmial.com>
2024-03-23 13:11:22 +0100

    quick fix names

--- Finansik/Components/Pages/HistoryPage.xaml.cs
+++ Finansik/Components/Pages/HistoryPage.xaml.cs
@@ -1,19 +1,13 @@
-using Finansik.Classes;
 using Finansik.Service;
 using Finansik.Service.DTO;
 using System.Collections.ObjectModel;
-using Finansik.Service.DTO;
 namespace Finansik.Components.Pages;
  
 public partial class HistoryPage : ContentPage
 {
-    public class Fruit
-    {
-        public string FruitName { get; set; }
-    }
     List<Finanse> finanses = new List<Finanse>();
-    ObservableCollection<Finanse> fruits = new ObservableCollection<Finanse>();
-    public ObservableCollection<Finanse> Fruits { get { return fruits; } }
+    ObservableCollection<Finanse> _finanses = new ObservableCollection<Finanse>();
+    public ObservableCollection<Finanse> Finanses { get { return _finanses; } }
 
     public HistoryPage()
 	{
@@ -25,9 +19,9 @@ public partial class HistoryPage : ContentPage
     {
         foreach (Finanse f in finanses)
         {
-            fruits.Add(AddFinanse(f.Title, f.Price));
+            _finanses.Add(AddFinanse(f.Title, f.Price));
         }
-        FruitListView.ItemsSource = fruits;
+        FruitListView.ItemsSource = _finanses;
     }
     async void  loadTable() 
     {
